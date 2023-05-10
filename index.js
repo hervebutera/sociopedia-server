f248595c1b54459f91370ef9c2f3cb95d63cc7f0
Herve Butera <h.buterak@gmail.com>
2023-05-10 18:57:30 +0200

    feat(users): implement singup and login

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
@@ -8,8 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { error } from "console";
-// import register from "./controllers/auth.js";
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js"
 
 /* CONFIGURATION */
 const __filename = fileURLToPath(import.meta.url);
@@ -37,7 +37,10 @@ const storage = multer.diskStorage({
 const upload = multer({storage})
 
 /* ROUTES WITH FILES */
-// app.post("/auth/register", upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES*/
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
